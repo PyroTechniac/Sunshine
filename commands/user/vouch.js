7917da6f820a7b2c0b93c5211f690d89471e08d7
Owen Bellish <shadowguy224@live.com>
2019-01-21 18:56:13 -0500

    added base vouch command stuff

---
+++ commands/user/vouch.js
@@ -0,0 +1,24 @@
+const { Command } = require('../../structures/Structures');
+module.exports = class VouchCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'vouch',
+            group: 'user',
+            memberName: 'vouch',
+            guildOnly: true,
+            description: 'For vouching for other members!',
+            args: [
+                {
+                    key: 'newbie',
+                    prompt: 'Enter the newbie you want to vouch for',
+                    type: 'member',
+                },
+            ],
+        });
+    }
+    async run(message, { newbie }) {
+        const members = require('../../server/models');
+        const newbieEntry = members.findOne({ where: { newbieUserId: newbie.user.id } });
+        console.log(newbieEntry);
+    }
+};
