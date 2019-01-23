d6e0961a7fd822551d687d3a67070b110cc41b76
Owen Bellish <shadowguy224@live.com>
2019-01-23 14:22:48 -0500

    Added untested first message and pun command

---
+++ commands/fun/pun.js
@@ -0,0 +1,15 @@
+const puns = require('../../assets/json/pun');
+const { Command } = require('../../structures/Structures');
+module.exports = class PunCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'pun',
+            group: 'fun',
+            memberName: 'pun',
+            description: 'Responds with a random pun.',
+        });
+    }
+    run(message) {
+        return message.say(puns[Math.floor(Math.random() * puns.length)]);
+    }
+};
