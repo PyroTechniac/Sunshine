35116409b809206d7ea6a8c5b7c48d4f69268a1f
Owen Bellish <shadowguy224@live.com>
2019-01-23 14:28:53 -0500

    Added base embed maker command

---
+++ commands/fun/embed-maker.js
@@ -0,0 +1,18 @@
+const { Command } = require('../../structures/Structures');
+const { MessageEmbed } = require('discord.js');
+module.exports = class EmbedMakerCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'embed-maker',
+            aliases: ['embed', 'embedder'],
+            group: 'fun',
+            memberName: 'embed-maker',
+            description: 'An interactive embed maker!',
+            throttling: { usages: 1, duration: 30 },
+        });
+    }
+    async run(message) {
+        const embed = new MessageEmbed();
+        message.embed(embed);
+    }
+};
