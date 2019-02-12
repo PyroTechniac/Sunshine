e389085c9af0478545a5390f6518a202a044827a
Owen Bellish <shadowguy224@live.com>
2019-02-12 15:48:37 -0500

    adding commands and handlers

--- Sunshine.js
+++ Sunshine.js
@@ -1,8 +1,12 @@
 const config = require('./config');
 const { AkairoClient } = require('discord-akairo');
 const client = new AkairoClient({
-    ownerID: config.owners
+    ownerID: config.owners,
+    prefix: config.prefix,
+    commandDirectory: './commands/',
+    listenerDirectory: './listeners/'
 }, {
     disableEveryone: true,
     disabledEvents: ['TYPING_START']
 });
+client.login(config.token);
---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+const { Command } = require('discord-akairo');
+
+class PingCommand extends Command {
+    constructor() {
+        super('ping', {
+            aliases: ['ping']
+        });
+    }
+    exec(message) {
+        return message.reply('Pong!');
+    }
+}
+module.exports = PingCommand;
