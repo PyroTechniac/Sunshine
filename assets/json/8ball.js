e04782e523030750a6af085b13d1429f0c20e208
Owen Bellish <shadowguy224@live.com>
2019-02-13 13:27:23 -0500

    8ball command

---
+++ assets/json/8ball.js
@@ -0,0 +1,45 @@
+module.exports = [
+    // Positive
+    'It is certain',
+    'Without a doubt',
+    'You can count on it',
+    'Most likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Absolutely',
+    '👍',
+    '✅',
+
+    // Unsure
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    '... 🕵️‍ ...',
+    '🤔 🤷‍',
+    '🤐',
+    '😑',
+
+    // Negative
+    'Don\'t count on it',
+    'No way',
+    'My sources say no',
+    'Very doubtful',
+    'Chances aren\'t good',
+    '👎',
+    '🙅‍',
+
+    // Random / Fun
+    'Huh?',
+    'No hablo Inglés',
+    'Frankly, my dear, I don\'t give a damn',
+    'You can\'t handle the truth!',
+    '<https://youtu.be/dQw4w9WgXcQ>',
+    'Oprah',
+    'SOON. 🤖🔪',
+    'I made this for you ⛄️',
+    'Have a potato 🥔',
+    '🦄',
+    '👁 👁'
+];
