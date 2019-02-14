450c914612db864fa4e89c38782e23ce57464147
Owen Bellish <shadowguy224@live.com>
2019-02-14 11:52:12 -0500

    disabled rate limits

--- events/rateLimit.js
+++ events/rateLimit.js
@@ -3,6 +3,11 @@ const { Event, Colors } = require('klasa');
 const HEADER = new Colors({ text: 'red' }).format('[RATELIMIT]');
 
 module.exports = class extends Event {
+    constructor(...args) {
+        super(...args, {
+            enabled: false
+        });
+    }
     run({ timeout, limit, method, route }) {
         this.client.emit('verbose', [
             HEADER,
