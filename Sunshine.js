9cdf6301d86bdae85fe53ba0fd2b968877eaabc5
Owen Bellish <shadowguy224@live.com>
2019-02-13 23:24:34 -0500

    level system

--- Sunshine.js
+++ Sunshine.js
@@ -1,6 +1,14 @@
 const config = require('./config');
 const { Client } = require('klasa');
 Client.defaultUserSchema.add('TODOs', 'any', { array: true });
+Client.defaultUserSchema.add('level', 'Integer', {
+    default: 0,
+    configurable: false
+});
+Client.defaultUserSchema.add('experience', 'Integer', {
+    default: 0,
+    configurable: false
+});
 const client = new Client({
     ownerID: config.owner,
     prefix: config.prefix,
