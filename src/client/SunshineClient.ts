2ecbe1f86336d01ae83ba4908ea801d2f66d61a4
PyroTechniac <39341355+PyroTechniac@users.noreply.github.com>
2019-04-19 17:10:44 -0400

    removing db stuff

--- src/client/SunshineClient.ts
+++ src/client/SunshineClient.ts
@@ -1,14 +1,7 @@
 import { AkairoClient, CommandHandler, InhibitorHandler, ListenerHandler } from 'discord-akairo';
 import { join } from 'path';
-import database from '../structures/Database';
-import { Connection } from 'typeorm';
-import TypeORMProvider from '../structures/SettingsProvider';
-import { Setting } from '../models/Settings';
 
 export default class SunshineClient extends AkairoClient {
-    public db!: Connection;
-
-    public settings!: TypeORMProvider;
 
     public commandHandler: CommandHandler = new CommandHandler(this, {
         directory: join(__dirname, '..', 'commands')
@@ -41,11 +34,6 @@ export default class SunshineClient extends AkairoClient {
         this.commandHandler.loadAll();
         this.inhibitorHandler.loadAll();
         this.listenerHandler.loadAll();
-
-        this.db = database.get('sunshine');
-        await this.db.connect();
-        // this.settings = new TypeORMProvider(this.db.getRepository(Setting));
-        // await this.settings.init();
     }
 
     public async start(): Promise<string> {
